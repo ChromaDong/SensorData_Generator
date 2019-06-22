123e52bbb249a5b6a98e21b045d26ce86d346f83
charlie_j107 <charlie_j107@outlook.com>
2019-06-22 20:21:00 +0800

    add notes.

--- CNNLearn/train.py
+++ CNNLearn/train.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from tensorflow import keras
 
-
+print("import finished")
 def read_and_decode(filename): # 读入tfrecords
     filename_queue = tf.train.string_input_producer([filename],shuffle=True)#生成一个queue队列
  
@@ -20,26 +20,43 @@ def read_and_decode(filename): # 读入tfrecords
  
     return img, label
 
-x = "训练样本"  # 二维矩阵 每一行是一个样本 列数为样本个数
+print("Defined finished")
+
+x = "训练样本"  
+print("# 二维矩阵 每一行是一个样本 列数为样本个数")
 y = "训练样本对应标签"
+
+print("start to read dataset and decode...")
 
 x,y=read_and_decode("./data.tfrecord")
 
+print("二维矩阵 每一行是对应样本取到该分类的概率 形如[0,...,0,1,0,...,0] 列数为样本个数 现假设样本有10个分类 那么列数就是10")
 
-# 二维矩阵 每一行是对应样本取到该分类的概率 形如[0,...,0,1,0,...,0] 列数为样本个数 现假设样本有10个分类 那么列数就是10
+print("start define model")
+
 model = keras.Sequential()
 
-# 32为该层神经元个数 activation为激活函数
+print("32为该层神经元个数 activation为激活函数")
+
 model.add(keras.layers.Dense(32, activation="sigmoid"))
 model.add(keras.layers.Dense(16, activation="sigmoid"))
 model.add(keras.layers.Dense(12, activation="softplus"))
-model.add(keras.layers.Dense(10, activation="softmax"))  # 最后输出长度为10的向量
+model.add(keras.layers.Dense(10, activation="softmax"))  
+
+print("最后输出长度为10的向量")
+
+print("Start compline")
 
 model.compile(optimizer=keras.optimizers.Adam(0.0001),
               loss="categorical_crossentropy", metrics=['accuracy'])
-# 编译模型 使用Adam优化器 如果用梯度下降可以改成 keras.optimizers.SGD 0.01是学习速率 损失函数是交叉熵
+print("编译模型 使用Adam优化器 如果用梯度下降可以改成 keras.optimizers.SGD 0.01是学习速率 损失函数是交叉熵")
+
+print("start to train")
 
 model.fit(x, y, eopchs=10, batch_size=20)
 # 训练模型 一共20步 采用随机小批量 批量个数为20
+print("train finished")
+
+print("save model")
 
 model.save("./")
