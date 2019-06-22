da4ec59621ddb7a6038e0c77cd0a65ce143cf3ed
charlie_j107 <charlie_j107@outlook.com>
2019-06-22 12:15:31 +0800

    Change MakeDataset.py to prepare for origin VM.

--- CNNLearn/makeDataSet.py
+++ CNNLearn/makeDataSet.py
@@ -14,28 +14,31 @@ import argparse
 # Import os.path for file path manipulation
 import os.path
 
-cwd='./rawData/'
-classes={'bow','sit','stand'}  #人为设定2类
+import os 
+import tensorflow as tf 
+from PIL import Image  
+import matplotlib.pyplot as plt 
+import numpy as np
+ 
+#cwd='./data/train/'
+cwd='./data/'
+classes={'sit','bow','stand'}  #人为设定2类
 #writer= tf.python_io.TFRecordWriter("dog_and_cat_train.tfrecords") #要生成的文件
-writer = tf.python_io.TFRecordWriter("data.tfrecords") #要生成的文件
+writer= tf.python_io.TFRecordWriter("data.tfrecords") #要生成的文件
 
 for index,name in enumerate(classes):
-    bagPath=cwd+name+'/'
-	#Create pipeline
-	pipeline = rs.pipeline()
-	# Create a config object
-	config = rs.config()
-	# Tell config that we will use a recorded device from filem to be used by the pipeline through playback.
-	rs.config.enable_device_from_file(config, bagPath)
-	# Configure the pipeline to stream the depth stream
-	config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
-
-	# Start streaming from file
-	pipeline.start(config)
-	frames = pipeline.wait_for_frames()
-    example = tf.train.Example(features=tf.train.Features(feature={
-        "label": tf.train.Feature(int64_list=tf.train.Int64List(value=[index])),
-        'img_raw': tf.train.Feature(bytes_list=tf.train.BytesList(value=[frame]))
+    class_path=cwd+name+'/'
+    for img_name in os.listdir(class_path): 
+        img_path=class_path+img_name #每一个图片的地址
+ 
+        img=Image.open(img_path)
+        img= img.resize((128,128))
+        print(np.shape(img))
+        img_raw=img.tobytes()#将图片转化为二进制格式
+        example = tf.train.Example(features=tf.train.Features(feature={
+            "label": tf.train.Feature(int64_list=tf.train.Int64List(value=[index])),
+            'img_raw': tf.train.Feature(bytes_list=tf.train.BytesList(value=[img_raw]))
         })) #example对象对label和image数据进行封装
-    writer.write(example.SerializeToString())  #序列化为字符串
+        writer.write(example.SerializeToString())  #序列化为字符串
+ 
 writer.close()
