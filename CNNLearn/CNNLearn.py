12560dc7afa8eed7a47acf6f3754ba3cfecd9a05
charlie_j107 <charlie_j107@outlook.com>
2019-06-22 14:21:49 +0800

    Code the CNN part of file.

--- CNNLearn/CNNLearn.py
+++ CNNLearn/CNNLearn.py
@@ -6,8 +6,25 @@ import tensorflow as tf
 
 from tensorflow.keras import datasets, layers, models
 
+def read_and_decode(filename): # 读入tfrecords
+    filename_queue = tf.train.string_input_producer([filename],shuffle=True)#生成一个queue队列
+ 
+    reader = tf.TFRecordReader()
+    _, serialized_example = reader.read(filename_queue)#返回文件名和文件
+    features = tf.parse_single_example(serialized_example,
+                                       features={
+                                           'label': tf.FixedLenFeature([], tf.int64),
+                                           'img_raw' : tf.FixedLenFeature([], tf.string),
+                                       })#将image数据和label取出来
+ 
+    img = tf.decode_raw(features['img_raw'], tf.uint8)
+    img = tf.reshape(img, [128, 128, 3])  #reshape为128*128的3通道图片
+    img = tf.cast(img, tf.float32) * (1. / 255) - 0.5 #在流中抛出img张量
+    label = tf.cast(features['label'], tf.int32) #在流中抛出label张量
+ 
+    return img, label
 
-(train_images, train_labels), (test_images, test_labels) = np_image
+(train_images, train_labels), (test_images, test_labels) = read_and_decode('/data/home/charlie/source/repos/SensorData_Generator/CNNLearn/data.tfrecords')
 
 
 train_images = train_images.reshape((60000, 28, 28, 1))
