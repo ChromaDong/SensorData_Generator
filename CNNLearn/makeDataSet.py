34ac731f72429e1e7aa7e347845dadf55e86c48e
charlie_j107 <charlie_j107@outlook.com>
2019-06-22 12:21:29 +0800

    Why it dosn't changed?

--- CNNLearn/makeDataSet.py
+++ CNNLearn/makeDataSet.py
@@ -1,30 +1,14 @@
 import os 
 import tensorflow as tf 
-import numpy as np
-#cwd='./data/train/'
-
-# First import library
-import pyrealsense2 as rs
-# Import Numpy for easy array manipulation
-import numpy as np
-# Import OpenCV for easy image rendering
-import cv2
-# Import argparse for command-line options
-import argparse
-# Import os.path for file path manipulation
-import os.path
-
-import os 
-import tensorflow as tf 
 from PIL import Image  
 import matplotlib.pyplot as plt 
 import numpy as np
  
 #cwd='./data/train/'
-cwd='./data/'
-classes={'sit','bow','stand'}  #人为设定2类
+cwd='./data/test/'
+classes={'dog','cat'}  #人为设定2类
 #writer= tf.python_io.TFRecordWriter("dog_and_cat_train.tfrecords") #要生成的文件
-writer= tf.python_io.TFRecordWriter("data.tfrecords") #要生成的文件
+writer= tf.python_io.TFRecordWriter("dog_and_cat_test.tfrecords") #要生成的文件
 
 for index,name in enumerate(classes):
     class_path=cwd+name+'/'
